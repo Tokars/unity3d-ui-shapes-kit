eb76bcba12a615721744430e131926c09e9302ad
tokar <tokar.dev@gmail.com>
2023-07-13 19:43:59 +0200

    init upm

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# ChangeLog
+
+
+## 0.0.1 - 2023-07-13
+- Package created
---
+++ Runtime/MinNumAttribute.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace ThisOtherThing.Utils
+{
+	public class MinNumAttribute : PropertyAttribute
+	{
+		public readonly float minFloat;
+		public readonly int minInt;
+
+		public MinNumAttribute(float min)
+		{
+			this.minFloat = min;
+		}
+
+		public MinNumAttribute(int min)
+		{
+			this.minInt = min;
+		}
+	}
+}
