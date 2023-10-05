4adaa52b65f37d3c2fe3c20d2d58aacb6fd12f39
tokar <tokar.dev@gmail.com>
2023-10-05 11:48:46 +0200

    fix: editor edge gradient

--- Editor/Editors/EdgeGradientEditor.cs
+++ Editor/Editors/EdgeGradientEditor.cs
@@ -8,18 +8,20 @@ namespace UIShapeKit.Editor.Editors
 	[CustomEditor(typeof(EdgeGradient))]
 	[CanEditMultipleObjects]
 	public class EdgeGradientEditor : GraphicEditor
-	{
+    {
+        private EdgeGradient _edgeGradient;
 		protected SerializedProperty materialProp;
 		protected SerializedProperty raycastTargetProp;
 
 		protected SerializedProperty propertiesProp;
 
 		protected override void OnEnable()
-		{
+        {
+            _edgeGradient = (EdgeGradient)target;
 			materialProp = serializedObject.FindProperty("m_Material");
 			raycastTargetProp = serializedObject.FindProperty("m_RaycastTarget");
 
-			propertiesProp = serializedObject.FindProperty("Properties");
+			propertiesProp = serializedObject.FindProperty(nameof(_edgeGradient.properties));
 		}
 
 		protected override void OnDisable()
--- Runtime/Shapes/EdgeGradient.cs
+++ Runtime/Shapes/EdgeGradient.cs
@@ -3,180 +3,178 @@ using UnityEngine.UI;
 
 namespace UIShapeKit.Shapes
 {
+    [AddComponentMenu("UI/Shapes/Edge Gradient", 100), RequireComponent(typeof(CanvasRenderer))]
+    public class EdgeGradient : MaskableGraphic, IShape
+    {
+        public enum Positions
+        {
+            Top,
+            Bottom,
+            Left,
+            Right,
 
-	[AddComponentMenu("UI/Shapes/Edge Gradient", 100), RequireComponent(typeof(CanvasRenderer))]
-	public class EdgeGradient : MaskableGraphic, IShape
-	{
-		public enum Positions
-		{
-			Top,
-			Bottom,
-			Left,
-			Right,
+            OuterTop,
+            OuterBottom,
+            OuterLeft,
+            OuterRight
+        }
 
-			OuterTop,
-			OuterBottom,
-			OuterLeft,
-			OuterRight
-		}
+        [SerializeField] public GradientProperty[] properties = {new()};
 
-		public GradientProperty[] Properties = { new GradientProperty()};
+        [System.Serializable]
+        public class GradientProperty
+        {
+            public float Size = 20.0f;
+            public Color32 Color = new Color32(127, 127, 127, 255);
 
-		[System.Serializable]
-		public class GradientProperty
-		{
-			public float Size = 20.0f;
-			public Color32 Color = new Color32(127, 127, 127, 255);
+            public Positions Position = Positions.Top;
+        }
 
-			public Positions Position = Positions.Top;
-		}
+        Vector3 topLeft = Vector3.zero;
+        Color32 gradientColor = new Color32(127, 127, 127, 255);
 
-		Vector3 topLeft = Vector3.zero;
-		Color32 gradientColor = new Color32(127, 127, 127, 255);
+        public void ForceMeshUpdate()
+        {
+            SetVerticesDirty();
+            SetMaterialDirty();
+        }
 
-		public void ForceMeshUpdate()
-		{
-			SetVerticesDirty();
-			SetMaterialDirty();
-		}
+        protected override void OnPopulateMesh(VertexHelper vh)
+        {
+            vh.Clear();
 
-		protected override void OnPopulateMesh(VertexHelper vh)
-		{
-			vh.Clear();
+            Rect pixelRect = RectTransformUtility.PixelAdjustRect(rectTransform, canvas);
 
-			Rect pixelRect = RectTransformUtility.PixelAdjustRect(rectTransform, canvas);
+            for (int i = 0; i < properties.Length; i++)
+            {
+                gradientColor.r = properties[i].Color.r;
+                gradientColor.g = properties[i].Color.g;
+                gradientColor.b = properties[i].Color.b;
+                gradientColor.a = 0;
 
-			for (int i = 0; i < Properties.Length; i++)
-			{
+                switch (properties[i].Position)
+                {
+                    case Positions.Top:
+                        topLeft.x = pixelRect.xMin;
+                        topLeft.y = pixelRect.yMax;
 
-				gradientColor.r = Properties[i].Color.r;
-				gradientColor.g = Properties[i].Color.g;
-				gradientColor.b = Properties[i].Color.b;
-				gradientColor.a = 0;
+                        ShapeUtils.Rects.AddVerticalTwoColorRect(
+                            ref vh,
+                            topLeft,
+                            properties[i].Size,
+                            pixelRect.width,
+                            properties[i].Color,
+                            gradientColor,
+                            GeoUtils.ZeroV2
+                        );
+                        break;
 
-				switch (Properties[i].Position)
-				{
-					case Positions.Top:
-						topLeft.x = pixelRect.xMin;
-						topLeft.y = pixelRect.yMax;
+                    case Positions.Bottom:
+                        topLeft.x = pixelRect.xMin;
+                        topLeft.y = pixelRect.yMin + properties[i].Size;
 
-						ShapeUtils.Rects.AddVerticalTwoColorRect(
-							ref vh,
-							topLeft,
-							Properties[i].Size,
-							pixelRect.width,
-							Properties[i].Color,
-							gradientColor,
-							GeoUtils.ZeroV2
-						);
-						break;
+                        ShapeUtils.Rects.AddVerticalTwoColorRect(
+                            ref vh,
+                            topLeft,
+                            properties[i].Size,
+                            pixelRect.width,
+                            gradientColor,
+                            properties[i].Color,
+                            GeoUtils.ZeroV2
+                        );
+                        break;
 
-					case Positions.Bottom:
-						topLeft.x = pixelRect.xMin;
-						topLeft.y = pixelRect.yMin + Properties[i].Size;
+                    case Positions.Left:
+                        topLeft.x = pixelRect.xMin;
+                        topLeft.y = pixelRect.yMax;
 
-						ShapeUtils.Rects.AddVerticalTwoColorRect(
-							ref vh,
-							topLeft,
-							Properties[i].Size,
-							pixelRect.width,
-							gradientColor,
-							Properties[i].Color,
-							GeoUtils.ZeroV2
-						);
-						break;
+                        ShapeUtils.Rects.AddHorizontalTwoColorRect(
+                            ref vh,
+                            topLeft,
+                            pixelRect.height,
+                            properties[i].Size,
+                            properties[i].Color,
+                            gradientColor,
+                            GeoUtils.ZeroV2
+                        );
+                        break;
 
-					case Positions.Left:
-						topLeft.x = pixelRect.xMin;
-						topLeft.y = pixelRect.yMax;
+                    case Positions.Right:
+                        topLeft.x = pixelRect.xMax - properties[i].Size;
+                        topLeft.y = pixelRect.yMax;
 
-						ShapeUtils.Rects.AddHorizontalTwoColorRect(
-							ref vh,
-							topLeft,
-							pixelRect.height,
-							Properties[i].Size,
-							Properties[i].Color,
-							gradientColor,
-							GeoUtils.ZeroV2
-						);
-						break;
+                        ShapeUtils.Rects.AddHorizontalTwoColorRect(
+                            ref vh,
+                            topLeft,
+                            pixelRect.height,
+                            properties[i].Size,
+                            gradientColor,
+                            properties[i].Color,
+                            GeoUtils.ZeroV2
+                        );
+                        break;
 
-					case Positions.Right:
-						topLeft.x = pixelRect.xMax - Properties[i].Size;
-						topLeft.y = pixelRect.yMax;
+                    case Positions.OuterTop:
+                        topLeft.x = pixelRect.xMin;
+                        topLeft.y = pixelRect.yMax + properties[i].Size;
 
-						ShapeUtils.Rects.AddHorizontalTwoColorRect(
-							ref vh,
-							topLeft,
-							pixelRect.height,
-							Properties[i].Size,
-							gradientColor,
-							Properties[i].Color,
-							GeoUtils.ZeroV2
-						);
-						break;
+                        ShapeUtils.Rects.AddVerticalTwoColorRect(
+                            ref vh,
+                            topLeft,
+                            properties[i].Size,
+                            pixelRect.width,
+                            gradientColor,
+                            properties[i].Color,
+                            GeoUtils.ZeroV2
+                        );
+                        break;
 
-					case Positions.OuterTop:
-						topLeft.x = pixelRect.xMin;
-						topLeft.y = pixelRect.yMax + Properties[i].Size;
+                    case Positions.OuterBottom:
+                        topLeft.x = pixelRect.xMin;
+                        topLeft.y = pixelRect.yMin;
 
-						ShapeUtils.Rects.AddVerticalTwoColorRect(
-							ref vh,
-							topLeft,
-							Properties[i].Size,
-							pixelRect.width,
-							gradientColor,
-							Properties[i].Color,
-							GeoUtils.ZeroV2
-						);
-						break;
+                        ShapeUtils.Rects.AddVerticalTwoColorRect(
+                            ref vh,
+                            topLeft,
+                            properties[i].Size,
+                            pixelRect.width,
+                            properties[i].Color,
+                            gradientColor,
+                            GeoUtils.ZeroV2
+                        );
+                        break;
 
-					case Positions.OuterBottom:
-						topLeft.x = pixelRect.xMin;
-						topLeft.y = pixelRect.yMin;
+                    case Positions.OuterLeft:
+                        topLeft.x = pixelRect.xMin - properties[i].Size;
+                        topLeft.y = pixelRect.yMax;
 
-						ShapeUtils.Rects.AddVerticalTwoColorRect(
-							ref vh,
-							topLeft,
-							Properties[i].Size,
-							pixelRect.width,
-							Properties[i].Color,
-							gradientColor,
-							GeoUtils.ZeroV2
-						);
-						break;
+                        ShapeUtils.Rects.AddHorizontalTwoColorRect(
+                            ref vh,
+                            topLeft,
+                            pixelRect.height,
+                            properties[i].Size,
+                            gradientColor,
+                            properties[i].Color,
+                            GeoUtils.ZeroV2
+                        );
+                        break;
 
-					case Positions.OuterLeft:
-						topLeft.x = pixelRect.xMin - Properties[i].Size;
-						topLeft.y = pixelRect.yMax;
+                    case Positions.OuterRight:
+                        topLeft.x = pixelRect.xMax;
+                        topLeft.y = pixelRect.yMax;
 
-						ShapeUtils.Rects.AddHorizontalTwoColorRect(
-							ref vh,
-							topLeft,
-							pixelRect.height,
-							Properties[i].Size,
-							gradientColor,
-							Properties[i].Color,
-							GeoUtils.ZeroV2
-						);
-						break;
-
-					case Positions.OuterRight:
-						topLeft.x = pixelRect.xMax;
-						topLeft.y = pixelRect.yMax;
-
-						ShapeUtils.Rects.AddHorizontalTwoColorRect(
-							ref vh,
-							topLeft,
-							pixelRect.height,
-							Properties[i].Size,
-							Properties[i].Color,
-							gradientColor,
-							GeoUtils.ZeroV2
-						);
-						break;
-				}
-			}
-		}
-	}
+                        ShapeUtils.Rects.AddHorizontalTwoColorRect(
+                            ref vh,
+                            topLeft,
+                            pixelRect.height,
+                            properties[i].Size,
+                            properties[i].Color,
+                            gradientColor,
+                            GeoUtils.ZeroV2
+                        );
+                        break;
+                }
+            }
+        }
+    }
 }
